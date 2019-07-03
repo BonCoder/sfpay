4ad3e2b87c4444c39b5e4f3efad071a577f83bc8
root <root@localhost.localdomain>
2019-07-03 23:12:09 +0800

    test

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -7,6 +7,7 @@ use think\Request;
 use app\admin\model\ChongZhiModel;
 use app\admin\model\DaifuModel;
 use app\admin\model\DaoruModel;
+use app\admin\model\MemberModel;
 
 class Index extends Controller
 {
@@ -25,28 +26,22 @@ class Index extends Controller
     public function host(Request $request)
     {
         $config = cache('db_config_data');
-        if ($request->isPost()) {
-            $data = $request->post();
-        } elseif ($request->isGet()) {
-            $data = $request->get();
-        } else {
-            $info = file_get_contents("php://input");
-            $data = json_decode($info);
-        }
+        $info = file_get_contents("php://input");
+        $data = json_decode($info);
         if (!$data){
             return json(['code' => 0, 'msg' => '参数不能为空！']);
         }
         $model = new DaifuModel();
         $model->member_id = $config['member_id'];
         $model->daoru_id = 1;
-        $model->money = -$data['amount'] / 100;
-        $model->create_time = strtotime($data['transDate']);
-        $statue = $data['transState'] == '00' ? 5 : 2;
+        $model->money = -$data->amount / 100;
+        $model->create_time = strtotime($data->transDate);
+        $statue = $data->transState == '00' ? 5 : 2;
         $model->status = $statue;
-        $model->shenfenzheng = $data['idNumber'];
-        $model->bank_card = $data['cardNumber'];
-        $model->bank_owner = $data['cardName'];
-        $model->bank_name = $data['cardName'];
+        $model->shenfenzheng = $data->idNumber;
+        $model->bank_card = $data->cardNumber;
+        $model->bank_owner = $data->cardName;
+        $model->bank_name = $data->cardName;
         $model->save();
 
         $chongzhi = new ChongZhiModel();
@@ -55,9 +50,13 @@ class Index extends Controller
         $chongzhi->member_id = $config['member_id'];
         $chongzhi->beizhu = "接口记录";
         $chongzhi->daoru_id = 1;
-        $chongzhi->create_time = strtotime($data['transDate']);
+        $chongzhi->create_time = strtotime($data->transDate);
         $chongzhi->save();
-
+		
+		$user = MemberModel::where('id', $config['member_id'])->find();
+        $user->money = $user->money - $model->money;
+        $user->save();
+		
         return json(['code' => 1, 'msg' => '插入成功！']);
     }
 }
