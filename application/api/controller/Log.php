2c47b899e7ccfcaff9681b7aa8896e54070f8038
BobCoder <bob@bobcoder.cc>
2019-07-29 17:24:29 +0800

    chore: 添加前台接口

---
+++ application/api/controller/Log.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @classname Log
+ * @author   Bob<bob@bobcoder.cc>
+ * @version   1$
+ * @link https://www.bobcoder.cc/
+ * @Date  2019/7/29
+ */
+
+namespace app\api\controller;
+
+
+use app\admin\model\LogModel;
+
+class Log extends Base
+{
+    /**
+     * 日志列表
+     *
+     * @param LogModel $log
+     * @return \think\response\Json|\think\response\Jsonp
+     * @throws \think\exception\DbException
+     * @link https://www.bobcoder.cc/
+     * @Date 2019/7/29
+     * @author Bob<bob@bobcoder.cc>
+     */
+    public function lists(LogModel $log)
+    {
+        $get = $this->request->get();
+        $limit = (int)$this->request->get('limit', 5);
+        $offset = (int)$this->request->get('offset', 0);
+        $map = [];
+        if (isset($get['admin_name']) && $get['admin_name']) {
+            $map['admin_name'] = ['like', "%" . $get['admin_name'] . "%"];
+        }
+        $list = $log
+            ->where($map)
+            ->order('create_time','desc')
+            ->limit($offset, $limit)
+            ->select();
+
+        return $this->sendJson($list);
+    }
+
+    /**
+     * 删除日志
+     *
+     * @param LogModel $log
+     * @return \think\response\Json
+     * @author Bob<bob@bobcoder.cc>
+     * @link https://www.bobcoder.cc/
+     * @Date 2019/7/29
+     */
+    public function del_log(LogModel $log)
+    {
+        $log_id = input('param.log_id');
+        $flag = $log->delLog($log_id);
+
+        return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+    }
+}
